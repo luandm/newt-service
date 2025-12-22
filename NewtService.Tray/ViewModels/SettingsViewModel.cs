49407ab1b3710a4672a46a2313e3e2bc76e521cd
memesalot <10216384+memesalot@users.noreply.github.com>
2025-12-22 00:11:52 -0800

    Add Start on Boot functionality to SettingsViewModel and update UI

--- NewtService.Tray/ViewModels/SettingsViewModel.cs
+++ NewtService.Tray/ViewModels/SettingsViewModel.cs
@@ -1,11 +1,16 @@
+using Microsoft.Win32;
 using NewtService.Core;
 
 namespace NewtService.Tray.ViewModels;
 
 public class SettingsViewModel : ViewModelBase
 {
+    private const string RunKey = @"Software\Microsoft\Windows\CurrentVersion\Run";
+    private const string AppName = "NewtService";
+    
     private bool _delayedStart;
     private bool _isInstalled;
+    private bool _startOnBoot;
 
     public bool DelayedStart
     {
@@ -25,6 +30,18 @@ public class SettingsViewModel : ViewModelBase
         set => SetProperty(ref _isInstalled, value);
     }
 
+    public bool StartOnBoot
+    {
+        get => _startOnBoot;
+        set
+        {
+            if (SetProperty(ref _startOnBoot, value))
+            {
+                SetStartOnBoot(value);
+            }
+        }
+    }
+
     public SettingsViewModel()
     {
         IsInstalled = ServiceControlHelper.IsServiceInstalled();
@@ -32,5 +49,42 @@ public class SettingsViewModel : ViewModelBase
         {
             _delayedStart = ServiceControlHelper.GetDelayedStart();
         }
+        _startOnBoot = GetStartOnBoot();
+    }
+
+    private bool GetStartOnBoot()
+    {
+        try
+        {
+            using var key = Registry.CurrentUser.OpenSubKey(RunKey, false);
+            return key?.GetValue(AppName) != null;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    private void SetStartOnBoot(bool enable)
+    {
+        try
+        {
+            using var key = Registry.CurrentUser.OpenSubKey(RunKey, true);
+            if (key == null) return;
+
+            if (enable)
+            {
+                var exePath = System.Diagnostics.Process.GetCurrentProcess().MainModule?.FileName;
+                if (!string.IsNullOrEmpty(exePath))
+                {
+                    key.SetValue(AppName, $"\"{exePath}\"");
+                }
+            }
+            else
+            {
+                key.DeleteValue(AppName, false);
+            }
+        }
+        catch { }
     }
 }
